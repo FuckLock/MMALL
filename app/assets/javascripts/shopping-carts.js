4f5bbd7560216987df85dfd3506c702e07bdb3c5
baodongdong <1214063293@qq.com>
2017-10-08 17:47:20 +0800

    js

--- app/assets/javascripts/shopping-carts.js
+++ app/assets/javascripts/shopping-carts.js
@@ -1,18 +1,17 @@
 $(function(){
 	$('.all-check').each(function(){
 		$(this).on('click', function(){
-			// window.alert("bao")
 			$(".all-check").each(function(){
 				var allValue = $(this).val();
 				if (allValue == 1){
 					$(this).removeProp("checked");
 					$(this).val(0);
+					$(".cart-concon").css("background", "");
 				}else {
 					$(this).prop("checked","checked");
-					// window.alert("bao");
 					$(this).val(1);
-				}
-				
+					$(".cart-concon").css("background", "#fff4e8");
+				};
 			});
 
 			var allValue = $(".all-check:first").val();
@@ -34,8 +33,7 @@ $(function(){
 				if (allValue == 0 && onlyValue == 1){
 					$(this).removeProp("checked");
 					$(this).val(0);
-				};				
-
+				};
 			});
 		});
 	});
@@ -44,6 +42,7 @@ $(function(){
 		var checkValue = $(this).val();
 		if (checkValue == 1){
 			$(this).prop("checked", "checked");
+			$(".cart-concon").css("background", "#fff4e8");
 		}
 	});
 
@@ -53,14 +52,18 @@ $(function(){
 			if (onlyValue == 1){
 				$(this).removeProp("checked");
 				$(this).val(0);
+				$(this).parent().parent().css("background", "");
 			}else {
 				$(this).prop("checked");
 				$(this).val(1);
+				$(this).parent().parent().css("background", "#fff4e8");
 			};
+
 			$('.all-check').each(function(){
 				$(this).removeProp("checked");
 				$(this).val(0);
 			});
+
 			var onlyValueSize = $(".only-value").size();
 			var checkedSize = $(".only-value:checked").size();
 			if (onlyValueSize == checkedSize) {
@@ -70,5 +73,19 @@ $(function(){
 				});
 			}
 		});
+	});
+
+	$(".tt-add").on('click', function(){
+		var addValue = $(this).prev().val();
+		var pid = $(this).next().val();
+		var price = parseInt($(this).parent().prev().text());
+		// window.alert(parseInt(price).toFixed(1));
+		addValue = parseInt(addValue) + 1;
+		total_price = price * addValue
+		$(this).prev().val(addValue);
+		$(this).parent().next().html('<i class="fa fa-jpy fa-sm" aria-hidden="true"></i>' + total_price.toFixed(1));
+		$.post('shopping_carts/add_amount', {amount: addValue, product_id: pid}, function(){
+
+		})
 	});
 });
--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -35,6 +35,11 @@ class ShoppingCartsController < ApplicationController
     redirect_to shopping_carts_path
   end
 
+  def add_amount
+    shopping_cart = ShoppingCart.find_by product_id: params[:product_id]
+    shopping_cart.update_attributes!(amount: params[:amount])
+  end
+
   private
   def find_shopping_cart
     @shopping_cart = ShoppingCart.find params[:id]
